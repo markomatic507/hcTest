c22007379b43ed935aeb97fb61cb3af80e988149
marko.matic <marko.matic507@gmail.com>
2021-08-24 01:06:22 +0200

    Added connection to mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,20 @@
 const app = require("./server");
+const mongoose = require("mongoose");
 
-app.listen(8080, () => {
-  console.log("Server on port", 8080);
-});
+const startUp = async () => {
+  try {
+    await mongoose.connect("mongodb://scrape-mongo-srv:27017/scrape", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(8080, () => {
+    console.log("Server on port", 8080);
+  });
+};
+
+startUp();
