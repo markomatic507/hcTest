0cfb2a3442e3f296c02386fe02c73b662ec21160
marko.matic <marko.matic507@gmail.com>
2021-08-24 21:15:44 +0200

    Added getting all data

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
 const scrape = require("../util/scrape");
+const Scraped = require("../models/scraped");
 
 router.post("/api/scrape", scrape);
+
+router.get("/api/scrape", (req, res) => {
+  const name = req.query.name;
+  var condition = name
+    ? { name: { $regex: new RegExp(name), $options: "i" } }
+    : {};
+
+  Scraped.find(condition)
+    .then((data) => {
+      res.send({ data: data });
+    })
+    .catch((err) => {
+      return res.status(500).send({ message: "Error" });
+    });
+});
 
 module.exports = router;
