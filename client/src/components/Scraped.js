ca210147329fd4782bd2b4674efa8837a9962793
marko.matic <marko.matic507@gmail.com>
2021-08-25 18:55:25 +0200

    Added scaped component for displaying scraped data

---
+++ client/src/components/Scraped.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import scrapeService from "../services/scrapeService";
+import DataCard from "./DataCard";
+
+const Scraped = () => {
+  const [scraped, setScraped] = useState();
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    retriveScraped();
+  }, []);
+
+  const onChangeSearh = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const retriveScraped = () => {
+    setError(false);
+    scrapeService
+      .getAll()
+      .then((res) => {
+        setScraped(res.data.scraped);
+        console.log(res.data.scraped);
+      })
+      .catch(() => setError(true));
+  };
+
+  const find = () => {
+    setError(false);
+    scrapeService
+      .getByName(search)
+      .then((res) => {
+        setScraped(res.data.scraped);
+        console.log(res.data.scraped);
+      })
+      .catch(() => setError(true));
+  };
+
+  return (
+    <div className='container'>
+      <div className='input-group'>
+        <input
+          type='text'
+          className='form-control'
+          id='search'
+          required
+          value={search}
+          onChange={onChangeSearh}
+          name='search'
+        />
+        <button
+          type='button'
+          className='btn btn-outline-secondary'
+          onClick={find}>
+          Search
+        </button>
+      </div>
+      <div class='mt-5 row row-cols-1 row-cols-md-2 g-4'>
+        {scraped &&
+          scraped.map((scrapeData) => <DataCard scrapeData={scrapeData} />)}
+      </div>
+
+      {error ? (
+        <div className='mt-5 text-center'>
+          <h4>Error has occured</h4>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default Scraped;
